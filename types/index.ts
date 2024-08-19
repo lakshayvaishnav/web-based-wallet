01182633bbcbc5ab8c801a704a05a00d8cb2d529
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-08-19 12:13:01 +0530

    added support for recoil

---
+++ types/index.ts
@@ -0,0 +1,22 @@
+export interface SolWallet {
+  id: number;
+  accountId: number;
+  publicKey: string;
+  createdAt: Date;
+}
+
+export interface EthWallet {
+  id: number;
+  accountId: number;
+  publicKey: string;
+  createdAt: Date;
+}
+
+export interface Account {
+  id: number;
+  userId: number;
+  name: string;
+  solWallet?: SolWallet | null;
+  ethWallet?: EthWallet | null;
+  createdAt: Date;
+}
