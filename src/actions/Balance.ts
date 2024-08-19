f2180a7f8d717759cbaed31e5bdd2ae771b646a3
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-08-19 14:08:21 +0530

    added actions for prisma

---
+++ src/actions/Balance.ts
@@ -0,0 +1,31 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+import { ethers } from "ethers";
+
+const solanaURL = process.env.NEXT_PUBLIC_SOLANA_URL!;
+const ethereumURL = process.env.NEXT_PUBLIC_ETHEREUM_URL!;
+
+const solanaConnection = new Connection(solanaURL);
+
+export async function getSOLBalance(publicKey: string) {
+  try {
+    const balance = await solanaConnection.getBalance(new PublicKey(publicKey));
+
+    return balance / 1e9;
+  } catch (err) {
+    console.error("Error fetching SOL balance:", err);
+    return 0;
+  }
+}
+
+const ethProvider = new ethers.JsonRpcProvider(ethereumURL);
+
+export async function getETHBalance(address: string) {
+  try {
+    const balance = await ethProvider.getBalance(address);
+
+    return ethers.formatEther(balance);
+  } catch (err) {
+    console.error("Error fetching ETH balance:", err);
+    return "0.00";
+  }
+}
