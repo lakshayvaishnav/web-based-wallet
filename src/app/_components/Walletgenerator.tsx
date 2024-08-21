2f160a2a3d88eea9d3e459d59edf465407b259f7
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-08-22 00:46:14 +0530

    mnemonic dispaly added

--- src/app/_components/Walletgenerator.tsx
+++ src/app/_components/Walletgenerator.tsx
@@ -10,6 +10,7 @@ import { ethers } from "ethers";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { ChevronDown, ChevronUp, Copy } from "lucide-react";
 interface Wallet {
   publickey: string;
   privatekey: string;
@@ -287,6 +288,72 @@ const WalletGenerator = () => {
       )}
 
       {/* Display secret Phrase */}
+      {mnemonicWords && wallets.length > 0 && (
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 0.3,
+            ease: "easeInOut",
+          }}
+          className="group flex flex-col items-center gap-4 cursor-pointer rounded-lg border border-primary/10 p-8"
+        >
+          <div
+            className="flex w-full justify-between items-center"
+            onClick={() => setShowMnemonic(!showMnemonic)}
+          >
+            <h2>Your Secret Phrase</h2>
+            <Button
+              onClick={() => setShowMnemonic(!showMnemonic)}
+              variant="ghost"
+            >
+              {showMnemonic ? (
+                <ChevronUp className="size-4" />
+              ) : (
+                <ChevronDown className="size-4" />
+              )}
+            </Button>
+          </div>
+
+          {showMnemonic && (
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{
+                duration: 0.3,
+                ease: "easeInOut",
+              }}
+              className="flex flex-col w-full items-center justify-center"
+              onClick={() => copyToClipboard(mnemonicWords.join(" "))}
+            >
+              <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.3,
+                  ease: "easeInOut",
+                }}
+                className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 justify-center w-full items-center mx-auto my-8"
+              >
+                {mnemonicWords.map((word, index) => (
+                  <p
+                    key={index}
+                    className="md:text-lg bg-foreground/5 hover:bg-foreground/10 transition-all duration-300 rounded-lg p-4"
+                  >
+                    {word}
+                  </p>
+                ))}
+              </motion.div>
+              <div className="text-sm md:text-base text-primary/50 flex w-full gap-2 items-center group-hover:text-primary/800 transition-all duration-300">
+                <Copy className="size-4" />
+                Click Anywhere To Copy
+              </div>
+            </motion.div>
+          )}
+        </motion.div>
+      )}
+
+      {/* display wallet pairs */}
     </div>
   );
 };
