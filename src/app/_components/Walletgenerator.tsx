c6640bf2026d231f1cbed19fa7d9ed89a7e5576d
thelakshayvaishnav <thelakshayvaishnav@gmail.com>
2024-08-21 23:11:58 +0530

    added generate wallet function with better logic

--- src/app/_components/Walletgenerator.tsx
+++ src/app/_components/Walletgenerator.tsx
@@ -1,7 +1,12 @@
 "use client";
-
+import { Keypair } from "@solana/web3.js";
+import { generateMnemonic, mnemonicToSeedSync, validateMnemonic } from "bip39";
+import { derivePath } from "ed25519-hd-key";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
+import nacl from "tweetnacl";
+import bs58 from "bs58";
+import { ethers } from "ethers";
 interface Wallet {
   publickey: string;
   privatekey: string;
@@ -82,5 +87,50 @@ const WalletGenerator = () => {
     setVisiblePhrases(
       visiblePhrases.map((visible, i) => (i === index ? !visible : visible))
     );
+  };
+
+  const generateWalletFromMnemonic = (
+    pathType: string,
+    mnemonic: string,
+    accountIndex: number
+  ): Wallet | null => {
+    try {
+      const seedBuffer = mnemonicToSeedSync(mnemonic);
+      const path = `m/44'/${pathType}'/0'/${accountIndex}'`;
+      const { key: derivedSeed } = derivePath(path, seedBuffer.toString("hex"));
+
+      let publicKeyEncoded: string;
+      let privateKeyEncoded: string;
+
+      if (pathType === "501") {
+        //solana
+
+        const { secretKey } = nacl.sign.keyPair.fromSeed(derivedSeed);
+        const keypair = Keypair.fromSecretKey(secretKey);
+
+        privateKeyEncoded = bs58.encode(secretKey);
+        publicKeyEncoded = keypair.publicKey.toBase58();
+      } else if (pathType === "60") {
+        // Ethereum
+        const privatekey = Buffer.from(derivedSeed).toString("hex");
+        privateKeyEncoded = privatekey;
+
+        const wallet = new ethers.Wallet(privatekey);
+        publicKeyEncoded = wallet.address;
+      } else {
+        toast.error("Unsupported path type.`");
+        return null;
+      }
+
+      return {
+        publickey: publicKeyEncoded,
+        privatekey: privateKeyEncoded,
+        mnemonic,
+        path,
+      };
+    } catch (error) {
+      toast.error("Failed to generate wallet. Please try again");
+      return null;
+    }
   };
 };
